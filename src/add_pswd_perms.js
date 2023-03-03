7da2960f9666010b29ef33d971e12f637fed06f6
Obi1kan <danielgribinet1234@gmail.com>
2023-03-03 15:05:55 +0200

    set passwords and permission

--- src/add_pswd_perms.js
+++ src/add_pswd_perms.js
@@ -2,14 +2,35 @@ import { faker } from '@faker-js/faker';
 
 const userUrl = "http://localhost:3000/users";
 let users = [];
+
+async function updateData(users, userUrl) {
+  for (let user of users) {
+      let url = userUrl + `/${user.id}`
+      await fetch(url, {
+          method: 'PATCH',
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify(user),
+      });
+
+      await new Promise(resolve => setTimeout(resolve, 100));
+  }
+}
+
 fetch(userUrl)
     .then((response) => {
     return response.json();
   })
-    .then((data) => {
+    .then(async (data) => {
     users = data;
-    for (let i = 0; i < users.length; i++){
-        users[i].password = faker.internet.password();
-        users[i].permission = faker.helpers.arrayElement(["admin", "user"]);
+    
+    for (let user of users){
+      user.password = faker.internet.password();
+      user.permission = faker.helpers.arrayElement(["admin", "user"]);
     }
+    
+    console.log(users[1]);
+
+    updateData(users, userUrl);
   })
