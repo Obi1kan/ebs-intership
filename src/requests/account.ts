cfe52e2c543b8d689167bbee4a1b3c561ab1b79f
Obi1kan <danielgribinet1234@gmail.com>
2023-03-09 17:58:09 +0200

    get data about user direct from mainAxios.patch

--- src/requests/account.ts
+++ src/requests/account.ts
@@ -20,8 +20,7 @@ router.patch('/', async (req, res) => {
   let token = req.headers.authorization!;
   let decoded = jwt.verify(token, secret) as Token;
   let id = decoded.id;
-  await mainAxios.patch(`users/${id}`, req.body);
-  let user = (await mainAxios.get(`users/${id}`)).data;
+  let user = await (await mainAxios.patch(`users/${id}`, req.body)).data;
   res.json(user);
 });
 
