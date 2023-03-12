3edc578344c3eb0f936d2d39a576c074aea56071
Obi1kan <danielgribinet1234@gmail.com>
2023-03-12 19:55:48 +0200

    send request from login page to server

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -3,18 +3,19 @@ import bodyParser from 'body-parser';
 import auth from './requests/auth';
 import account from './requests/account';
 import dotenv from 'dotenv';
-
+import cors from 'cors';
 dotenv.config();
 const port = process.env.PORT;
 const server = jsonServer.create();
 const router = jsonServer.router('src/db.json');
 const middlewares = jsonServer.defaults();
 
+server.use(cors());
 server.use(bodyParser.json());
 server.use('/auth', auth);
 server.use('/account', account);
 server.use(middlewares);
 server.use(router);
-server.listen(port, () => {
+server.listen(3002, () => {
   console.log('JSON Server is running');
 });
--- packages/user-interface/src/pages/login.tsx
+++ packages/user-interface/src/pages/login.tsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import './styles/login.css'
+import { mainAxios } from "../utils/mainAxios";
 
 function Login(){
+    
+    const [user, setUser] = useState({
+        username: '',
+        password: ''
+    })
+
+    const handleInput = (event: any) => {
+        setUser({...user, [event.target.name]: event.target.value})
+    }
+
+    async function handleSubmit (event: any){
+        event.preventDefault();
+        
+        mainAxios.post('/auth/login', user)
+        .then(response => console.log(response))
+        .catch(err => console.log(err))
+    }
+
     return (
         <div id="login">
-            <form>
-  <label htmlFor="username">Username:</label>
-  <input type="text" id="username" name="username" placeholder="Enter your username"/>
-  <label htmlFor="password">Password:</label>
-  <input type="password" id="password" name="password" placeholder="Enter your password"/>
-  <input type="submit" value="Submit"/>
-</form>
-
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="username">Username:</label>
+                <input type="text" onChange={handleInput} id="username" name="username" placeholder="Enter your username"/>
+                <label htmlFor="password">Password:</label>
+                <input type="password" onChange={handleInput} id="password" name="password" placeholder="Enter your password"/>
+                <input type="submit" value="Submit"/>
+            </form>
         </div>
     )
 }
